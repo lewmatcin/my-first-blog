b30b44e10d2dea7aee09a7df8d88c2752c9e98f5
marcinl <marcinl@poczta.pl>
2020-01-02 11:59:44 +0100

    Allow for making and managing drafts

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,7 @@ from .models import Post
 
 def post_list(request: HttpRequest) -> HttpResponse:
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+
     return render(request=request,
                   template_name='blog/post_list.html',
                   context={'posts': posts})
@@ -19,6 +20,7 @@ def post_list(request: HttpRequest) -> HttpResponse:
 
 def post_detail(request: HttpRequest, pk: int) -> HttpResponse:
     post = get_object_or_404(klass=Post, pk=pk)
+
     return render(request=request,
                   template_name='blog/post_detail.html',
                   context={'post': post})
@@ -30,11 +32,11 @@ def post_new(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect(to='post_detail', pk=post.pk)
     else:
         form = PostForm()
+
     return render(request=request,
                   template_name='blog/post_edit.html',
                   context={'form': form})
@@ -47,11 +49,26 @@ def post_edit(request: HttpRequest, pk: int) -> HttpResponse:
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect(to='post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
+
     return render(request=request,
                   template_name='blog/post_edit.html',
                   context={'form': form})
+
+
+def post_draft_list(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+    return render(request=request,
+                  template_name='blog/post_draft_list.html',
+                  context={'posts': posts})
+
+
+def post_publish(request: HttpRequest, pk: int) -> HttpResponse:
+    post = get_object_or_404(klass=Post, pk=pk)
+    post.publish()
+
+    return redirect(to='post_detail', pk=pk)
